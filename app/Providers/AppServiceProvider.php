38a20a296c33d5aa094ecb1ced5f1efbfcc5d46b
Daffa954 <124135109+Daffa954@users.noreply.github.com>
2026-05-05 03:20:02 +0700

    add prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL; // 👈 1. Tambahkan baris ini di atas
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,5 +20,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
